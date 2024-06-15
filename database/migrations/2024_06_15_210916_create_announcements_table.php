2513593c5c1c48ebd6fe889751bc270c5206bc52
zakkturner <zakkturner@yahoo.com>
2024-06-15 17:59:06 -0400

    start of announcement form

---
+++ database/migrations/2024_06_15_210916_create_announcements_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('announcements', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('is_visible')->default(false);
+            $table->string("body");
+            $table->string("phone");
+            $table->string("facebook_url");
+            $table->string("twitter_url");
+            $table->string("instagram_url");
+            $table->string("youtube_url");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('announcements');
+    }
+};
--- resources/js/Pages/Admin/Announcements/Create.vue
+++ resources/js/Pages/Admin/Announcements/Create.vue
@@ -1,9 +1,35 @@
-<script setup>
+<script lang="ts"  setup>
+
+import {ModelRef} from "vue";
+
+const body: ModelRef<string> = defineModel()
+function countChars(){
+    if (body.value)
+    {
+        return 150 - body.value.length;
+
+    }
+    return 150;
+   // charCount = textArea.value.value.length;
+}
+
 
 </script>
 
 <template>
-<div>Create</div>
+<div>
+    <div>Create Announcement</div>
+    <form>
+        <div class="flex flex-col">
+            <label>Body ( <span :class="{'text-red-500': countChars() < 5 }">{{countChars()}}  Characters Left </span>) {{countChars() < 5}} </label>
+            <textarea @keyup="countChars" v-model="body" rows="10" cols="30" maxlength="150"></textarea>
+        </div>
+        <div>
+
+            <input type="text" name="">
+        </div>
+    </form>
+</div>
 </template>
 
 <style scoped>
