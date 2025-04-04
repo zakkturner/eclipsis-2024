defa2f39a1c9580bc1dd6a746f7d6c274fb79347
zakkturner <zakkturner@yahoo.com>
2025-04-03 23:02:19 -0400

    border bug

--- resources/js/Components/NavLink.vue
+++ resources/js/Components/NavLink.vue
@@ -32,7 +32,7 @@ const handleLeave = () => {
 const classes = computed(() =>
     props.active
         ?
-        'inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 text-gray-900 focus:border-eclipsis-gold  transition duration-150 ease-in-out'
+        'inline-flex items-center px-1 pt-1 text-sm font-medium border-b-2 border-transparent  leading-5 text-gray-900 focus:border-eclipsis-gold  transition duration-150 ease-in-out'
         :
         'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-white  transition duration-150 ease-in-out'
 );
