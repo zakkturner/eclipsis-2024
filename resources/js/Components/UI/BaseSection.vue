0a2487520971333a656716cbed96aed0466a846d
zakkturner <zakkturner@yahoo.com>
2025-04-21 23:32:28 -0400

    base section created

---
+++ resources/js/Components/UI/BaseSection.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+
+import {computed} from "vue";
+
+const props = defineProps<{
+  color?: string
+}>()
+
+const bgColor = computed(() => {
+  switch (props.color) {
+    case 'blue':
+      return 'bg-eclipsis-navy'
+      break;
+    case 'gray':
+      return 'bg-gray-100'
+      break;
+    default:
+      return 'bg-white'
+
+  }
+})
+
+</script>
+
+
+<template>
+  <section :class="`py-20 ${bgColor}`">
+    <slot name="title"/>
+    <div class="max-w-[90%] xl:max-w-6xl mx-auto py-20">
+      <slot/>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
