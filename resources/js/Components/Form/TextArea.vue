97c10111f8cfc7c70ad6b8e9f7a38df79c370d7e
zakkturner <zakkturner@yahoo.com>
2025-06-25 22:12:28 -0400

    fixed sign-up color

--- resources/js/Components/Form/TextArea.vue
+++ resources/js/Components/Form/TextArea.vue
@@ -1,25 +1,25 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import {onMounted, ref} from 'vue';
 
 const model = defineModel({
-    type: String,
-    required: true,
+  type: String,
+  required: true,
 });
 
 const input = ref(null);
 
 onMounted(() => {
-    if (input.value.hasAttribute('autofocus')) {
-        input.value.focus();
-    }
+  if (input.value.hasAttribute('autofocus')) {
+    input.value.focus();
+  }
 });
 
-defineExpose({ focus: () => input.value.focus() });
+defineExpose({focus: () => input.value.focus()});
 </script>
 
 <template>
     <textarea
-        class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+        class="border-gray-300 text-eclipsis-navy focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
         v-model="model"
         ref="input">
     </textarea>
